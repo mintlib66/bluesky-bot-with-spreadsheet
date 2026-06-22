ad6a47be1e5e6217fafb5e9a257a4f4f6c500110
hy <mintlib66@gmail.com>
2026-06-22 12:04:02 +0900

    feat: 구글 스프레드시트 연결 및 스레드 게시처리

--- src/post.ts
+++ src/post.ts
@@ -1,4 +1,5 @@
 import { BskyAgent, RichText } from "@atproto/api";
+import Papa from "papaparse";
 
 async function main() {
   const identifier = process.env.BLUESKY_IDENTIFIER;
@@ -16,16 +17,78 @@ async function main() {
   });
   console.log("로그인:", identifier);
 
-  const text = `봇 작동 테스트\n실행:${new Date().toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })}`;
-  const richText = new RichText({ text });
-  await richText.detectFacets(agent);
+  //데이터 불러오기(csv export)
+  const sheetId = process.env.SHEET_ID;
+  const gid = process.env.SHEET_GID;
+  const url =
+    `https://docs.google.com/spreadsheets/d/${sheetId}/export?` +
+    `format=csv&gid=${gid}`;
 
-  const res = await agent.post({
-    text: richText.text,
-    facets: richText.facets,
-    createdAt: new Date().toISOString(),
-  });
-  console.log("게시 완료-- URI: ", res.uri, " / CID: ", res.cid);
+  const csv = await fetch(url).then((r) => r.text());
+
+  //파싱처리
+  const sheetData = Papa.parse(csv, { header: false });
+  // console.log(sheetData);
+  const dataArray = sheetData.data as string[][];
+
+  if (dataArray.length > 0) {
+    dataArray.forEach((row) => {
+      const date = row[0];
+      const today = new Date().toISOString().split("T")[0];
+      if (date == today) {
+        console.log("======================================");
+        console.log(date, today);
+        console.log(row);
+        console.log("======================================");
+        post(row[1], row[2]);
+      }
+    });
+  } else {
+    console.log("입력 데이터가 없습니다.");
+  }
+
+  //게시
+  async function post(postText: string, replyText?: string) {
+    const text =
+      postText +
+      `\n\n작동 테스트 실행:${new Date().toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })}`;
+    const richText = new RichText({ text: text });
+    await richText.detectFacets(agent);
+
+    //첫번째 글 게시
+    const res = await agent.post({
+      text: richText.text,
+      facets: richText.facets,
+      createdAt: new Date().toISOString(),
+    });
+    console.log("게시 완료-- URI: ", res.uri, " / CID: ", res.cid);
+
+    if (replyText) {
+      const secText =
+        replyText +
+        `\n\n작동 테스트 실행:${new Date().toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })}`;
+      const richText2 = new RichText({ text: secText });
+      await richText2.detectFacets(agent);
+
+      //스레드로 연결되는 추가 게시글 게시
+      const replyRes = await agent.post({
+        text: richText2.text,
+        reply: {
+          root: { uri: res.uri, cid: res.cid },
+          parent: { uri: res.uri, cid: res.cid },
+        },
+        facets: richText2.facets,
+        createdAt: new Date().toISOString(),
+      });
+
+      console.log(
+        "추가 게시 완료-- URI: ",
+        replyRes.uri,
+        " / CID: ",
+        replyRes.cid,
+      );
+    }
+  }
 }
 
 main().catch((err) => {
